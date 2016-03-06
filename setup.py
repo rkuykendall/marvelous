f8c30bad7544187d35c275ee9676994e59c9e0ac
Robert Kuykendall <robert@rkuykendall.com>
2016-03-06 16:34:40 -0500

    Clever get_requirements function doesn't work when requirements file not included on pypi.

--- setup.py
+++ setup.py
@@ -2,19 +2,6 @@ from setuptools import setup, find_packages
 
 version = '0.0.1'
 
-
-def get_requirements():
-    with open('requirements.txt') as f:
-        # git here should be remoted when the version of requests-cache
-        # in pypi supports ignored_parameters. Unitl then cache will not work
-        # without manually installing the version from github.
-        #
-        # TODO: Cache this missing module in the code and print instructions
-        #       for installing it.
-        return [
-            line for line in f.read().splitlines()
-            if 'nose' not in line and 'git' not in line]
-
 setup(
     name='marvelous',
     version='0.0.1',
@@ -24,6 +11,13 @@ setup(
     author_email='robert@rkuykendall.com',
     url='http://github.com/rkuykendall/marvelous',
     packages=find_packages(),
-    install_requires=get_requirements(),
     zip_safe=True,
+
+    # requests-cache should be added here when the version in pypi supports
+    # ignored_parameters. Unitl then cache will not work without manually
+    # installing the version from github.
+    #
+    # TODO: Cache this missing module in the code and print instructions
+    #       for installing it.
+    install_requires=['marshmallow', 'requests'],
     )
