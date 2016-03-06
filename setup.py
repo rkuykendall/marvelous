bb22ad367a858391f5429e2ff4a40c0859aea086
Robert Kuykendall <robert@rkuykendall.com>
2016-03-06 16:21:39 -0500

    Don't include cachine in pypi version yet.

--- setup.py
+++ setup.py
@@ -5,7 +5,15 @@ version = '0.0.1'
 
 def get_requirements():
     with open('requirements.txt') as f:
-        return [line for line in f.read().splitlines() if 'nose' not in line]
+        # git here should be remoted when the version of requests-cache
+        # in pypi supports ignored_parameters. Unitl then cache will not work
+        # without manually installing the version from github.
+        #
+        # TODO: Cache this missing module in the code and print instructions
+        #       for installing it.
+        return [
+            line for line in f.read().splitlines()
+            if 'nose' not in line and 'git' not in line]
 
 setup(
     name='marvelous',
